02a3a3c390b610a93bf809247bc5b8e0546a6efc
IvanSavDev <ivan-savenko@mail.ru>
2022-05-08 16:22:18 +0300

    refactor: change function for a new data

--- src/button.js
+++ src/button.js
@@ -1,19 +1,36 @@
-import { engLayout } from './symbol-keyboards';
+import { getLayout, keyboardCodeRows } from './symbol-keyboards';
 
-const sampleBtn = (numberRow, numberBtn, content) => `<div data-row="${numberRow + 1}" data-btn="${numberBtn + 1}" class="keyboard__button">
-  <span class="keyboard__content">${content}</span>
+const sampleBtn = (numberRow, numberBtn, content, code) => `<div data-row="${numberRow + 1}" data-btn="${numberBtn + 1}" data-code="${code}" class="keyboard__button">
+  <span class="keyboard__content">${content[code]}</span>
 </div>`;
 
 export const sampleContentBtn = (content) => `<span class="keyboard__content">${content}</span>`;
 
-export const generateButtons = (currentRow, currentLayout = engLayout) => currentLayout[currentRow]
-  .map((element, index) => sampleBtn(currentRow, index, element));
+const defLayout = getLayout();
+// let letterPosititon = 0;
+// const countLetter = [13, 14, 12, 12, 8];
 
-export const updateBtn = (currentRow, currentLayout) => currentLayout[currentRow]
-  .map((element) => sampleContentBtn(element));
+export const generateButtons = (currentRow, currentLayout = defLayout) => {
+  const rowCodes = keyboardCodeRows[currentRow];
+  return rowCodes.map((code, index) => sampleBtn(currentRow, index, currentLayout, code));
+};
+// countLetterInRows[currentRow].
+//   .map((element, index) => sampleBtn(currentRow, index, element, codeKeyboard));
 
-// <div data-row='1' class="keybord__button">
-//   <div class="keyboard-layout keyboard__lang--eng">
-//     <div data-btn='1' class="keyboard__content">tab</div>
-//   </div>
-// </div>
+// export const updateBtn = (currentRow, currentLayout) => currentLayout[currentRow]
+//   .map((element) => sampleContentBtn(element)); ?????????мей би была полезна
+
+//   const defLayout = getLayout();
+// let letterPosititon = 0;
+// const countLetter = [13, 14, 12, 12, 8];
+
+// export const generateButtons = (currentRow, currentLayout = defLayout) => {
+//   const currentRowLength = countLetter[currentRow];
+//   const btns = [];
+//   for (let i = 0; i <= currentRowLength; i += 1) {
+//     btns.push(sampleBtn(currentRow, i, currentLayout, letterPosititon));
+//     letterPosititon += 1;
+//   }
+
+//   return btns;
+// };
